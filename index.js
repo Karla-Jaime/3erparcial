a1a1d3a93b4106197ad4174a0f2feadbb97cb21c
Karla-Jaime <you@example.com>
2021-11-25 15:17:20 -0700

    cambios

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import * as THREE from './js/build/three.module.js';
-import {GLTFLoader} from './js/examples/jsm/loaders/GLTFLoader.js'
+import * as THREE from './dist/js/build/three.module.js';
+import {GLTFLoader} from './dist/js/examples/jsm/loaders/GLTFLoader.js'
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
